4870a0b0d9d251d3ca273e2b7786c64f71018c75
Arif Mammadov <arif.mmdv@gmail.com>
2026-06-30 00:12:37 +0300

    PWA Support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,9 @@
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png">
+
+        <link rel="manifest" href="/app.webmanifest">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=plus-jakarta-sans:400,500,600,700&display=swap" rel="stylesheet" />
